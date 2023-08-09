ad80c11921f55f42612ed03df6a4b43dd5db2f87
Uhan7 <luckyfellowfella@gmail.com>
2023-08-09 23:26:51 +0800

    Made the Meter half work:D

--- Assets/Scripts/LuksoTimer.cs
+++ Assets/Scripts/LuksoTimer.cs
@@ -7,24 +7,35 @@ public class LuksoTimer : MonoBehaviour
 
     private Rigidbody2D rb;
     private RectTransform rt;
+    private Animator anim;
 
     public float speed;
     private bool speedChanged;
+    private bool touching;
+
+    private bool broke;
+    public float brokeTime;
 
     public GameObject canvas;
     public GameObject[] FXs;
     public GameObject chosenFX;
+    public GameObject pulseFX;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rt = GetComponent<RectTransform>();
+        anim = GetComponent<Animator>();
 
         speedChanged = false;
+
     }
 
     private void Update()
     {
+
+        anim.SetBool("Broke", broke);
+
         if (rt.localPosition.x >= 146 && !speedChanged)
         {
             speed *= -1;
@@ -36,13 +47,28 @@ public class LuksoTimer : MonoBehaviour
             speedChanged = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && touching && !broke)
         {
             var FX = Instantiate(chosenFX) as GameObject;
             FX.transform.SetParent(canvas.transform, false);
             FX.transform.position = transform.position;
+
+            var PFX = Instantiate(pulseFX) as GameObject;
+            PFX.transform.SetParent(canvas.transform, false);
+            PFX.transform.position = new Vector2(160, transform.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && !touching)
+        {
+            StartCoroutine(Break());
         }
 
+    }
+
+    IEnumerator Break()
+    {
+        broke = true;
+        yield return new WaitForSeconds(brokeTime);
+        broke = false;
     }
 
     void FixedUpdate()
@@ -50,8 +76,11 @@ public class LuksoTimer : MonoBehaviour
         rb.velocity = (Vector2.right * speed);
     }
 
-    private void OnTriggerStay2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
+
+        touching = true;
+
         if (col.gameObject.CompareTag("Red Spot"))
         {
             chosenFX = FXs[0];
@@ -64,9 +93,9 @@ public class LuksoTimer : MonoBehaviour
         {
             chosenFX = FXs[2];
         }
-        else
-        {
-            print("nah");
-        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        touching = false;
     }
 }
