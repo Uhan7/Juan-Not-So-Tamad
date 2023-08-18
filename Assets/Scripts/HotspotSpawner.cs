18685fbe93faade3e1befa29d09bcb223cd02440
Uhan7 <luckyfellowfella@gmail.com>
2023-08-19 02:11:14 +0800

    TIMER SPAWNING WOWRKS

--- Assets/Scripts/HotspotSpawner.cs
+++ Assets/Scripts/HotspotSpawner.cs
@@ -12,27 +12,32 @@ public class HotspotSpawner : MonoBehaviour
 
     public GameObject[] hotspot;
 
+    public int[] wave;
+    public int[] loc;
+
     void Start()
     {
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(0, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(8, 146));
-        StartCoroutine(SpawnHotSpots(16, 146));
-        StartCoroutine(SpawnHotSpots(16, 146));
-        StartCoroutine(SpawnHotSpots(24, 146));
-        StartCoroutine(SpawnHotSpots(24, 146));
-        StartCoroutine(SpawnHotSpots(24, 146));
+
+        var location = new int[9];
+
+        location[0] = -146;
+        location[1] = -110;
+        location[2] = - 74;
+        location[3] = -37;
+        location[4] = 0;
+        location[5] = 37;
+        location[6] = 74;
+        location[7] = 110;
+        location[8] = 146;
+
+        var count = 0;
+        while (count < wave.Length)
+        {
+
+            StartCoroutine(SpawnHotSpots(wave[count], location[loc[count]]));
+            count++;
+        }
+
     }
 
     IEnumerator SpawnHotSpots(int time, float pos)
@@ -53,8 +58,6 @@ public class HotspotSpawner : MonoBehaviour
         }
         Hotspot.transform.SetParent(spawner.transform, false);
         Hotspot.transform.localPosition = new Vector2(pos, transform.position.y-162);
-
-        print("boom");
 
         yield return null;
     }
